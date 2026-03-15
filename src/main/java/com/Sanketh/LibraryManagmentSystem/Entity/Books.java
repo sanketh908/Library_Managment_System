47aa6e9f9f3ee2b025f5059b91f794c8fdec6d1f
sanketh908 <sankeths908@gmail.com>
2026-03-15 19:05:15 +0530

    Database configuration

--- src/main/java/com/Sanketh/LibraryManagmentSystem/Entity/Books.java
+++ src/main/java/com/Sanketh/LibraryManagmentSystem/Entity/Books.java
@@ -1,5 +1,32 @@
 package com.Sanketh.LibraryManagmentSystem.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+@Data
+@Entity
+@Table("books")
 public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    @Column(name = "title", nullable = false,unique = true)
+    private String title;
+    @Column(name = "author", nullable = false)
+    private String author;
+    @Column(name = "publisher", nullable = false)
+    private  String publisher;
+    @Column(name = "category", nullable = false)
+    private String category;
+    @Column(name = "isbn", nullable = false, unique = true)
+    private String isbn;
+    @Column(name = "published_date", nullable = false)
+    private LocalDate publishedDate;
+    @Column(name = "total_copies", nullable = false)
+    private String totalCopies;
+    @Column(name = "available_copies", nullable = false)
+    private int availableCopies;
 
 }
