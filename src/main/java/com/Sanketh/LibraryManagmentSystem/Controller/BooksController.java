a9bd1f4935a94e5b31cd2ece1ed58ad81df29662
sanketh908 <sankeths908@gmail.com>
2026-03-17 21:25:29 +0530

    Exception handling

--- src/main/java/com/Sanketh/LibraryManagmentSystem/Controller/BooksController.java
+++ src/main/java/com/Sanketh/LibraryManagmentSystem/Controller/BooksController.java
@@ -2,8 +2,9 @@ package com.Sanketh.LibraryManagmentSystem.Controller;
 
 import com.Sanketh.LibraryManagmentSystem.Service.BooksService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BooksController {
    private final BooksService booksService;
 
--- src/main/java/com/Sanketh/LibraryManagmentSystem/Rerpository/BooksRepository.java
+++ src/main/java/com/Sanketh/LibraryManagmentSystem/Rerpository/BooksRepository.java
@@ -2,11 +2,12 @@ package com.Sanketh.LibraryManagmentSystem.Rerpository;
 
 import com.Sanketh.LibraryManagmentSystem.Entity.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface BooksRepository extends JpaRepository<Books, Integer> {
   Optional<List<Books>> findAllBooksByAuthor(String author);
   Optional<List<Books>> findAllBooksPublishedDate(LocalDate publishedDate);
